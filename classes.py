9e1d6aef8aefe9bd2665fe2185e5a873c364c67a
Abdulkreem <shimatrusaesh@gmail.com>
2025-11-13 17:29:08 +0300

    nothing to commit, working tree clean

--- classes.py
+++ classes.py
@@ -1,1 +1,25 @@
-print("kgkgluglglg")
+# class subjects:
+#     def __init__(self, name, code,MaxCapacity,credits):
+#         self.name = name
+#         self.code = code
+#         self.MaxCapacity = MaxCapacity
+#         self.credits = credits
+
+#     def display_info(self):
+#         return f"Subject Name: {self.name}, Subject Code: {self.code}, Max Capacity: {self.MaxCapacity}, Credits: {self.credits}"
+    
+    
+
+# class students:
+#     def __init__(self, name, student_id,)
+
+class user:
+    def __init__(self, username, password, Id=None , email, status):
+        self.username = username
+        self.password = password
+        self.Id = Id
+        self.email = email
+        self.status = status
+
+    def display_info(self):
+        return f"Username: {self.username}, Role: {self.role}"
