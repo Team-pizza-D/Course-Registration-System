0ee75aa6f881c79bd6c5136d0bea800975b6ac65
Abdulkreem <shimatrusaesh@gmail.com>
2025-11-13 17:39:26 +0300

    classes under progress

--- classes.py
+++ classes.py
@@ -14,7 +14,7 @@
 #     def __init__(self, name, student_id,)
 
 class user:
-    def __init__(self, username, password,  email, status, Id=None ):
+    def __init__(self, username, password,  email, status="inactive", Id=None ):
         self.username = username
         self.password = password
         self.Id = Id
@@ -23,3 +23,26 @@ class user:
 
     def display_info(self):
         return f"Username: {self.username}, Email: {self.email}, Status: {self.status}, ID: {self.Id}"
+    def activate(self):
+        self.status = "active"
+
+
+class admin(user):
+    def __init__(self, username, password, email, status="active", Id=None):
+        super().__init__(username, password, email, status, Id)
+    def add_subject(self, subject_code):
+        pass
+    def remove_subject(self, subject_code, student_id):
+        pass
+    def view_all_students(self):
+        pass
+    def find_student(self, student_id):
+        pass
+    def view_all_subjects(self):
+        pass
+    def find_subject(self, subject_code):
+        pass
+    def expand_capacity(self, subject_code, new_capacity):
+        pass 
+
+
