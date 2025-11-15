de0f7999c1fa7ba0604fe6a070c94542eca1be0c
Abdulaziz <zoalqahtany1@gmail.com>
2025-11-15 05:10:38 +0300

    Nothing to say

--- Programs.py
+++ Programs.py
@@ -7,6 +7,7 @@ course_codes_Y2 = ["MATH206", "IE200","PHYS202", "CHEM281", "EE201", "ISLS101", 
 course_codes_Y3 = ["ARAB201", "EE202", "EE250", "MATH204", "ISLS201", "EE300", "EE301", "IE202"]
 course_codes_Y4 = ["EE321", "EE311", "IE256", "EE360", "EE366", "ISLS301"]
 course_codes_Y5 = ["EE499", "ISLS401", "EE390"]
+All = course_codes_Y1 + course_codes_Y2 + course_codes_Y3 + course_codes_Y4 + course_codes_Y5
 ####################################################################################################
 Y1_Credits = [3, 3, 3, 3, 3, 3, 2, 2]
 Y2_Credits = [4, 2, 4, 1, 2, 2, 2, 3, 3, 3, 4, 1]
@@ -33,11 +34,10 @@ communication = ["EE421", "EE423", "EE413", "EE425"]
 biomedical_sh = ["EE306", "EE302", "EE312", "EE351"]##################################################### Not shared with all majors
 biomedical = ["BIO321", "EE374", "EE372", "EE370", "EE472", "EE474", "EE471", "EE470"]
 
-### For whose working with databases , these are temporary values(only credits and prerequisites)
-### MMM & CCC & BBB are just temporary
-MMM = {"EE331": 0, "EE302": 0, "EE306": 0, "EE332": 0, "IE331": 0, "EE312": 0, "EE351": 0,}
-CCC = {"EE331": 0, "EE302": 0, "EE306": 0, "EE332": 0, "IE331": 0, "EE312": 0, "EE351": 0,}
-BBB = {"EE331": "Principles of Automatic Control",
+### For who is working with databases , these are temporary values(only credits and prerequisites)
+Semi_Shared_C = {"EE331": 0, "EE302": 0, "EE306": 0, "EE332": 0, "IE331": 0, "EE312": 0, "EE351": 0,}
+Semi_Shared_PR = {"EE331": 0, "EE302": 0, "EE306": 0, "EE332": 0, "IE331": 0, "EE312": 0, "EE351": 0,}
+Semi_Shared_Courses_Names = {"EE331": "Principles of Automatic Control",
        "EE302": "Magnetic 1", 
        "EE306": "Electrical Engineering Technologies", 
        "EE332": "Numerical Methods in Engineering", 
@@ -45,9 +45,39 @@ BBB = {"EE331": "Principles of Automatic Control",
        "EE312": "Electronics 2", 
        "EE351": "Electrical Power Systems I"}
 ###all the shared courses codes
-secondYear_courses = {"ARAB201":0, "EE202":0, "EE250":0, "MATH204":0, "ISLS201":0, "EE300":0, "EE301":0, "IE202":0}
-secondYear_courses2 = {"ARAB201":0, "EE202":0, "EE250":0, "MATH204":0, "ISLS201":0, "EE300":0, "EE301":0, "IE202":0}
-secondyear_coursesName= {"ARAB201":"Arabic Language 2",
+# C = credits
+# PR = prerequisites
+First_Year_C = {"MATH110": 3, "PHYS110": 3, "CHEM110": 3, "CPIT110": 3, "BIO110": 3,"STAT110": 3,"ELIS110": 3,"ELIS120": 3}
+First_Year_PR = {"MATH110": None, "PHYS110": None, "CHEM110": None, "CPIT110": None, "BIO110": None,"STAT110": None,"ELIS110": None,"ELIS120": None}
+First_Year_Courses_Names= {"MATH110":"General Mathematics 1",
+                        "PHYS110":"General Physics 1",
+                        "CHEM110":" General Chemistry 1",
+                        "CPIT110":"Problem Solving & Programming",
+                        "BIO110":"General Biology 1",
+                        "STAT110":"General Statistics 1",
+                        "ELIS110":"English Language-Science 1",
+                        "ELIS120":"English Language-Science 2"}
+
+Second_Year_C = {"MATH206": 4, "IE200": 2,"PHYS202": 4, "CHEM281": 1, "EE201": 2, "ISLS101": 2,
+                "ARAB101": 3, "IE201": 3, "IE255": 3, "MENG102": 3, "MATH207": 4, "PHYS281": 1}
+Second_Year_PR = {"MATH206": "MATH110", "IE200": None,"PHYS202": "MATH110" and "Phys110", "CHEM281": "CHEM110", "EE201": None, "ISLS101": None,
+                "ARAB101": None, "IE201": None, "IE255": None, "MENG102": None, "MATH207": "Math206", "PHYS281": None}
+Second_Year_Courses_Names= {"MATH206":"Calculus 2 for Engineers",
+                        "IE200":"Technical Communication Skills",
+                        "PHYS202":"General Physics 2",
+                        "CHEM281":"General Chemistry Lab",
+                        "EE201":"Struvtured Computer Programming",
+                        "ISLS101":"Islamic Culture 1",
+                        "ARAB101":"Arabic Language 1",
+                        "IE201":"Introduction to Engineering Design 1",
+                        "IE255":"Engineering Economy",
+                        "MENG102":"Engineering Graphics",
+                        "MATH207":"Calculus 3 for Engineers",
+                        "PHYS281":"General Physics Lab"}
+
+Third_Year_C = {"ARAB201":0, "EE202":0, "EE250":0, "MATH204":0, "ISLS201":0, "EE300":0, "EE301":0, "IE202":0}
+Third_Year_PR = {"ARAB201":0, "EE202":0, "EE250":0, "MATH204":0, "ISLS201":0, "EE300":0, "EE301":0, "IE202":0}
+Third_Year_Courses_Names= {"ARAB201":"Arabic Language 2",
                         "EE202":"Object-Oriented Computer Programming",
                         "EE250":"Basic Electrical Circuits",
                         "MATH204":"Differential Equations 1",
@@ -56,20 +86,24 @@ secondyear_coursesName= {"ARAB201":"Arabic Language 2",
                         "EE301":"Electrical Circuits and Systems",
                         "IE202":"Introduction to Engineering Design 2"}
 
-thirdYear_courses = {"EE321":0, "EE311":0, "IE256":0, "EE360":0, "EE366":0, "ISLS301":0}
-thirdYear_courses2 = {"EE321":0, "EE311":0, "IE256":0, "EE360":0, "EE366":0, "ISLS301":0}
-thirdyear_coursesName= {"EE321":"Introduction to Communications",  
+Fourth_Year_C = {"EE321":0, "EE311":0, "IE256":0, "EE360":0, "EE366":0, "ISLS301":0}
+Fourth_Year_PR = {"EE321":0, "EE311":0, "IE256":0, "EE360":0, "EE366":0, "ISLS301":0}
+Fourth_Year_Courses_Names= {"EE321":"Introduction to Communications",  
                        "EE311":"Electronics 1", 
                        "IE256":"Engineering Management", 
                        "EE360":"Digital Design 1", 
                        "EE366":"Microprocessors and Microcontrollers", 
                        "ISLS301":"Islamic Culture 3"}
 
-fourthYear_courses = {"EE499":0, "ISLS401":0, "EE390":0}
-fourthYear_courses2 = {"EE499":0, "ISLS401":0, "EE390":0}
-fourthyear_coursesName= {"EE499":"Senior Project",
+Fifth_Year_C = {"EE499":0, "ISLS401":0, "EE390":0} 
+Fifth_Year_PR = {"EE499":0, "ISLS401":0, "EE390":0} 
+Fifth_Year_Courses_Names= {"EE499":"Senior Project",
                          "ISLS401":"Islamic Culture 4", 
                          "EE390":"Summer Training"}
+
+All_Courses_C = {**First_Year_C, **Second_Year_C, **Third_Year_C, **Fourth_Year_C, **Fifth_Year_C}  
+All_Courses_PR = {**First_Year_PR, **Second_Year_PR, **Third_Year_PR, **Fourth_Year_PR, **Fifth_Year_PR}
+All_Courses_Names = {**First_Year_Courses_Names, **Second_Year_Courses_Names, **Third_Year_Courses_Names, **Fourth_Year_Courses_Names, **Fifth_Year_Courses_Names}
 # Power and machine courses :
 power_courses = {"MEP261":0, "EE303":0, "EE341":0, "EE442":0, "EE441":0, "EE404":0, "EE451":0, "EE405":0, "EE453":0, "EE454":0} 
 power_courses2 = {"MEP261":0, "EE303":0, "EE341":0, "EE442":0, "EE441":0, "EE404":0, "EE451":0, "EE405":0, "EE453":0, "EE454":0}
@@ -116,18 +150,6 @@ biomedical_coursesName= {"BIO321":"Biomedical Engineering",
 
 
 
-
-
-
-course_credits = {}
-course_prerequests = {}
-course_sections = {}
-course_section_codes = {}
-course_instructors = {}
-computer_capacitys = {}
-
-
-
 def loop_dict_key(d):
     x = []
     for key in d.keys():
@@ -139,18 +161,46 @@ def loop_dict_value(d):
     for value in d.values():
         y.append(value)
     return y
-A = loop_dict_value(computer_codes)
-B = loop_dict_key(course_codes)
-C = loop_dict_value(course_credits)
-D = loop_dict_value(course_prerequests)
 
-def add_course(x, y ,z, p):
+
+def computer(x1, y1, z1, p1):
     db = sqlite3.connect('courses.db')
     cr = db.cursor()
-    cr.execute('CREATE TABLE IF NOT EXISTS courses (course_name TEXT, course_code TEXT, credit INTEGER, prerequisites TEXT)')
-    for i in range(len(x)):
-        cr.execute('INSERT INTO courses (course_name, course_code, credit, prerequisites) VALUES (?, ?, ?, ?)',
-               (x[i] , y[i], z[i], p[i]))
+    cr.execute("DROP TABLE computer")
+    cr.execute('CREATE TABLE IF NOT EXISTS Computer (course_name TEXT, course_code TEXT, credit INTEGER, prerequisites TEXT)')
+    for i in range(len(x1)):
+        cr.execute('INSERT INTO Computer (course_name, course_code, credit, prerequisites) VALUES (?, ?, ?, ?)',
+               (x1[i] , y1[i], z1[i], p1[i]))
     db.commit()
     db.close()
-add_course(A, B, C, D)
+
+def power(x1, y1, z1, p1):
+    db = sqlite3.connect('courses.db')
+    cr = db.cursor()
+    cr.execute('CREATE TABLE IF NOT EXISTS Power (course_name TEXT, course_code TEXT, credit INTEGER, prerequisites TEXT)')
+    for i in range(len(x1)):
+        cr.execute('INSERT INTO IF NOT EXISTS Power (course_name, course_code, credit, prerequisites) VALUES (?, ?, ?, ?)',
+               (x1[i] , y1[i], z1[i], p1[i]))
+    db.commit()
+    db.close()
+
+def communication(x1, y1, z1, p1):
+    db = sqlite3.connect('courses.db')
+    cr = db.cursor()
+    cr.execute('CREATE TABLE IF NOT EXISTS Communication (course_name TEXT, course_code TEXT, credit INTEGER, prerequisites TEXT)')
+    for i in range(len(x1)):
+        cr.execute('INSERT INTO IF NOT EXISTS Communication (course_name, course_code, credit, prerequisites) VALUES (?, ?, ?, ?)',
+               (x1[i] , y1[i], z1[i], p1[i]))
+    db.commit()
+    db.close()
+
+def biomedical(x1, y1, z1, p1):
+    db = sqlite3.connect('courses.db')
+    cr = db.cursor()
+    cr.execute('CREATE TABLE IF NOT EXISTS Biomedical (course_name TEXT, course_code TEXT, credit INTEGER, prerequisites TEXT)')
+    for i in range(len(x1)):
+        cr.execute('INSERT INTO IF NOT EXISTS Biomedical (course_name, course_code, credit, prerequisites) VALUES (?, ?, ?, ?)',
+               (x1[i] , y1[i], z1[i], p1[i]))
+    db.commit()
+    db.close()
+
