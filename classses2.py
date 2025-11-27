6d1d9567b943079ec5d069b73c9beb02bc3ffaf4
Azad Felemban <you@example.com>
2025-11-28 00:37:09 +0300

    view enrolled subjects done

--- classses2.py
+++ classses2.py
@@ -663,15 +663,15 @@ class student(user):
         if row is None or len(row) == 0:
             return f"{self.id}, No enrolled subjects found"
         enrolled_sections = [r[0] for r in row]
-        row = courses_db.execute("SELECT course_code, course_name FROM Courses WHERE section IN ({seq})".format(seq=','.join(['?']*len(enrolled_sections))), tuple(enrolled_sections), fetchall=True)
-        enrolled_subjects = {r[0]: r[1] for r in row}
+        row = courses_db.execute("SELECT course_code, course_name,time FROM Courses WHERE section IN ({seq})".format(seq=','.join(['?']*len(enrolled_sections))), tuple(enrolled_sections), fetchall=True)
+        enrolled_subjects = {r[0]: (r[1], r[2]) for r in row}
         all_enrolled = {}
         for section in enrolled_sections:
             course_row = courses_db.execute("SELECT course_code FROM Courses WHERE section = ?", (section,), fetchone=True)
             if course_row:
                 course_code = course_row[0]
-                course_name = enrolled_subjects.get(course_code, "Unknown Course")
-                all_enrolled[section] = (course_code, course_name)
+                course_info = enrolled_subjects.get(course_code, ("Unknown Course", "No Time Info"))
+                all_enrolled[section] = (course_code, course_info[0], course_info[1])
         return all_enrolled
     
     def view_available_subjects(self):  # to view all available subjects for enrollment
