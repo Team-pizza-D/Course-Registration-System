76edbc0b4c1501afbe63288c224decd5ad1be046
Ammar <ammar.e.jeddawi@gmail.com.com>
2025-12-06 20:38:10 +0300

    Main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+
+from GUI import LoginWindow   
+
+def main():
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
